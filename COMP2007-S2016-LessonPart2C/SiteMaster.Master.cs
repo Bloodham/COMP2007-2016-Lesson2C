3f0600113fe4d7b36fa846e8aed4d79818565248
Bloodham <bloodham25@gmail.com>
2016-05-19 19:03:46 -0400

    Added active class method

--- COMP2007-S2016-LessonPart2C/SiteMaster.Master.cs
+++ COMP2007-S2016-LessonPart2C/SiteMaster.Master.cs
@@ -11,7 +11,40 @@ namespace COMP2007_S2016_LessonPart2C
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            addActiveClass();
 
+        }
+
+        /**
+         * This Method adds a class of active to the list items in 
+         * the main navarbar depending on page title
+         * 
+         * @method addActiveClass
+         * @return{void}
+         * 
+         */
+
+        private void addActiveClass()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Projects":
+                    projects.Attributes.Add("class", "active");
+                    break;
+                case "Services":
+                    services.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+                case "About":
+                    about.Attributes.Add("class", "active");
+                    break;
+            }
+            
         }
     }
 }
